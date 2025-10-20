4eefe272d63f458fb19ecea20d012c7eea8b7d81
Assim <assimben2004@hotmail.com>
2025-10-20 15:47:47 +0200

    Resolve merge conflict between improve-add and improve-subtract

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ def add(*args):
     """Return the sum of all numbers provided."""
     return sum(args)
 
+
 def subtract(*args):
     print("Running improved subtract function")
     result = args[0]
